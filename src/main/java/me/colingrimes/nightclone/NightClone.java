a99e104936a5a2e6bf9337930b1def2c9d8fca28
Scill <realscill@gmail.com>
2023-08-08 22:44:44 -0400

    Initial commit

---
+++ src/main/java/me/colingrimes/nightclone/NightClone.java
@@ -0,0 +1,11 @@
+package me.colingrimes.nightclone;
+
+import me.colingrimes.midnight.Midnight;
+
+public class NightClone extends Midnight {
+
+    @Override
+    protected void enable() {
+        super.enable();
+    }
+}
